8c2aa9415354eb95aefcc52afdfe38aa9248fd9b
Jacopo Ceccuti <jacopo.ceccuti@gmail.com>
2024-10-22 16:52:46 +0200

    Fixed regression with one hot encoded attributes

--- Regression_A.py
+++ Regression_A.py
@@ -1,36 +1,28 @@
 from ExtractData import *
 import matplotlib.pyplot as plt
 from sklearn import model_selection, linear_model as lm
-from dtuimldmtools import rlr_validate #train_neural_net, draw_neural_net, visualize_decision_boundary
+from dtuimldmtools import rlr_validate
 
-
-# print(df.head())
 # One-hot encoding categorical variables
-df_encoded = pd.get_dummies(df, columns=['Car Make', 'Country'], drop_first=True)
+df_encoded = pd.get_dummies(df, columns=['Car Make', 'Country'], drop_first=True, dtype=int)
+# print(df_encoded.describe(include='all'))
 
 # Extract target and features
 y = df_encoded['Price (in USD)'].to_numpy()
 X = df_encoded.drop(['Price (in USD)'], axis=1).to_numpy()
-
-# Handle NaNs and convert to float64
 X = np.nan_to_num(X).astype(float)
 
 # Add bias term (offset)
 X = np.hstack([np.ones((X.shape[0], 1)), X])
-
 attributeNames = ['Offset', 'Car Make', 'Year', 'Horsepower', 'Torque (lb-ft)', '0-60 MPH Time (seconds)', 'Country']
-
 N, M = X.shape
-
-
-
 
 ## Crossvalidation
 # Create crossvalidation partition for evaluation
 K = 10
 CV = model_selection.KFold(K, shuffle=True)
 
-lambdas = np.array([0, 10**-3, 10**-2, 10**-1, 10**1, 10**2, 10**3, 10**4, 10**5], dtype=float)
+lambdas = np.array([1e-8, 10**-3, 10**-2, 10**-1, 10**1, 10**2, 10**3, 10**4, 10**5], dtype=float)
 
 
 Error_train = np.empty((K,1))
@@ -63,14 +55,11 @@ for train_index, test_index in CV.split(X,y):
     mu[k, :] = np.mean(X_train[:, 1:], 0)
     sigma[k, :] = np.std(X_train[:, 1:], 0)
     
-    # Avoid division by zero by replacing 0 std with a small epsilon
     epsilon = 1e-8  # Small constant to prevent divide-by-zero errors
     safe_sigma = np.where(sigma[k, :] == 0, epsilon, sigma[k, :])
 
     X_train[:, 1:] = (X_train[:, 1:] - mu[k, :]) / safe_sigma
     X_test[:, 1:] = (X_test[:, 1:] - mu[k, :]) / safe_sigma
-    print(np.isnan(X_train).any(), np.isnan(X_test).any())
-
     
     Xty = X_train.T @ y_train
     XtX = X_train.T @ X_train
@@ -90,7 +79,10 @@ for train_index, test_index in CV.split(X,y):
     Error_test_rlr[k] = np.square(y_test-X_test @ w_rlr[:,k]).sum(axis=0)/y_test.shape[0]
 
     # Estimate weights for unregularized linear regression, on entire training set
-    w_noreg[:,k] = np.linalg.solve(XtX,Xty).squeeze()
+    
+    AvoidCrash = 1e-8 * np.eye(M)
+    AvoidCrash[0,0] = 0 # Do no regularize the bias term
+    w_noreg[:,k] = np.linalg.solve(XtX + AvoidCrash,Xty).squeeze() # TODO: ADD HERE SMALL REGULARIZATION TERM FOR NUMERICAL STABILITY
     # Compute mean squared error without regularization
     # Error_train[k] = np.square(y_train-X_train @ w_noreg[:,k]).sum(axis=0)/y_train.shape[0]
     # Error_test[k] = np.square(y_test-X_test @ w_noreg[:,k]).sum(axis=0)/y_test.shape[0]
@@ -101,23 +93,26 @@ for train_index, test_index in CV.split(X,y):
 
     # Display the results for the last cross-validation fold
     if k == K-1:
-        plt.figure(k, figsize=(12,8))
-        # subplot(1,2,1)
-        # semilogx(lambdas,mean_w_vs_lambda.T[:,1:],'.-') # Don't plot the bias term
-        # xlabel('Regularization factor')
-        # ylabel('Mean Coefficient Values')
-        # grid()
-        # # You can choose to display the legend, but it's omitted for a cleaner 
-        # # plot, since there are many attributes
-        # #legend(attributeNames[1:], loc='best')
+        plt.figure(k, figsize=(14,8))
+        plt.subplot(1,2,1)
+        plt.semilogx(lambdas,mean_w_vs_lambda.T[:,1:],'.-') # Don't plot the bias term
+        plt.xlabel('Regularization factor', fontweight="bold")
+        plt.ylabel('Mean Coefficient Values', fontweight="bold")
+        plt.title('Regularization factor vs Mean Coefficient Values', fontweight="bold")
+        plt.grid()
+        # You can choose to display the legend, but it's omitted for a cleaner 
+        # plot, since there are many attributes
+        plt.legend(attributeNames[1:], loc='best')
+
         
-        # subplot(1,2,2)
-        plt.title('Optimal lambda: 1e{0}'.format(np.log10(opt_lambda)))
+        plt.subplot(1,2,2)
+        plt.title('Optimal lambda: 1e{0}'.format(np.log10(opt_lambda)), fontweight="bold")
         plt.loglog(lambdas,train_err_vs_lambda.T,'b.-',lambdas,test_err_vs_lambda.T,'r.-')
-        plt.xlabel('Regularization factor')
-        plt.ylabel('Squared error (crossvalidation)')
+        plt.xlabel('Regularization factor', fontweight="bold")
+        plt.ylabel('Squared error (crossvalidation)', fontweight="bold")
         plt.legend(['Train error','Validation error'])
         plt.grid()
+        plt.suptitle("Regularization with Cross-Validation", fontsize=20, fontweight="bold", color="red")
         plt.show()
     
     # To inspect the used indices, use these print statements
